ed1157327660d9df33495a9c8542715c88216b7f
SauravMuke54 <saurav54muke@gmail.com>
2022-05-02 13:49:11 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WiFi-Attendance
+# WiFi-Attendance
---
+++ app/src/main/java/com/example/prroxy/wifiDirectBrodcastReciever.java
@@ -0,0 +1,56 @@
+package com.example.prroxy;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class wifiDirectBrodcastReciever extends BroadcastReceiver{
+
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+    private MainActivity activity;
+
+    public wifiDirectBrodcastReciever(WifiP2pManager manager, WifiP2pManager.Channel channel, MainActivity activity) {
+        this.manager = manager;
+        this.channel = channel;
+        this.activity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        String action = intent.getAction();
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            //CHECK TO SEE IF WIFI IS ENABLED AND NOTIFY APPROPRIATE ACTIVITY
+
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+            if (manager != null) {
+
+                if (ActivityCompat.checkSelfPermission(context.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this.activity,
+                            new String[] {ACCESS_FINE_LOCATION},
+                            1);
+                }
+                manager.requestPeers(channel, activity.peerListListener);
+            }
+            //CALL WIFIP2PMANAGER.requesrPeers() to get a list of current peers
+        }
+        else if(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action))
+        {
+            //Respond to new connection or disconnection
+        }
+    }
+
+
+}
